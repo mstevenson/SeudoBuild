f77e471b42d656878df36f286a7cf70df75f9dd1
Michael Stevenson <michael@mstevenson.net>
2016-11-04 12:26:03 -0700

    Added tests project

---
+++ UnityBuildServer.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace UnityBuildServer.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
