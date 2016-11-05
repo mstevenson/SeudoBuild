4420c3e9a01a16f479550bb84fd1b7a0e67baa9a
Michael Stevenson <michael@mstevenson.net>
2016-11-04 23:16:20 -0700

    Added configuration serializer

--- UnityBuildServer/Build/BuilderConfig.cs
+++ UnityBuildServer/Build/BuilderConfig.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace UnityBuildServer
 {
     public class BuilderConfig
     {
         public string ProjectsPath { get; set; }
+        public List<string> UnityInstallations { get; set; } = new List<string>();
     }
 }
---
+++ UnityBuildServer/ConfigSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace UnityBuildServer
+{
+    public class ConfigSerializer
+    {
+        public T Deserialize<T>(string path)
+        {
+            string json = File.ReadAllText(path);
+            T obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+
+        public void Serialize<T>(T obj, string path)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+    }
+}
