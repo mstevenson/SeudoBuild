504503587ad40a47cb5dc466d57b4eb422e42de9
Michael Stevenson <michael@mstevenson.net>
2016-11-07 21:35:00 -0800

    Start build server if agent is given no arguments

---
+++ SeudoBuild.Agent/BuildServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SeudoBuild.Agent
+{
+    public class BuildServer : IDisposable
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting server");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping server");
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
--- SeudoBuild.Agent/Program.cs
+++ SeudoBuild.Agent/Program.cs
@@ -17,7 +17,7 @@ namespace SeudoBuild.Agent
             {
                 { "o|output-path=", "a path to the build output folder", o => outputPath = o },
                 { "p|project-config=", "the path to a project configuration json file", p => projectConfigPath = p },
-                { "t|build-target=", "the name of the target to build", t => buildTarget = t }
+                { "t|build-target=", "the name of the target to build", t => buildTarget = t },
             };
 
             List<string> extra;
@@ -33,31 +33,30 @@ namespace SeudoBuild.Agent
 
             if (args.Length == 0)
             {
-                // TODO Begin listening for network messages
-                options.WriteOptionDescriptions(Console.Out);
+                var server = new BuildServer();
+                server.Start();
             }
             else
             {
-                ProjectConfig projectConfig = null;
-
-                if (!string.IsNullOrEmpty(projectConfigPath) && !string.IsNullOrEmpty(buildTarget) && !string.IsNullOrEmpty(outputPath))
-                {
-                    try
-                    {
-                        var s = new Serializer();
-                        projectConfig = s.Deserialize<ProjectConfig>(projectConfigPath);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Can't parse project config:");
-                        Console.WriteLine(e.Message);
-                    }
-                }
-                else
+                if (string.IsNullOrEmpty(projectConfigPath) || string.IsNullOrEmpty(buildTarget) || !string.IsNullOrEmpty(outputPath))
                 {
                     options.WriteOptionDescriptions(Console.Out);
+                    return;
                 }
-
+                
+                ProjectConfig projectConfig = null;
+                
+                try
+                {
+                    var s = new Serializer();
+                    projectConfig = s.Deserialize<ProjectConfig>(projectConfigPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Can't parse project config:");
+                    Console.WriteLine(e.Message);
+                }
+                
                 if (projectConfig != null)
                 {
                     BuilderConfig builderConfig = new BuilderConfig { ProjectsPath = outputPath };
