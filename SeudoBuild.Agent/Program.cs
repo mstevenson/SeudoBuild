7c00e67ac3346f44d6ba402cd7e9bd221e7b2c16
Michael Stevenson <michael@mstevenson.net>
2016-11-08 09:17:56 -0800

    Help text

--- SeudoBuild.Agent/Program.cs
+++ SeudoBuild.Agent/Program.cs
@@ -18,6 +18,20 @@ namespace SeudoBuild.Agent
 
             [VerbOption("listen", HelpText = "Listen for and fulfill build requests.")]
             public ListenSubOptions ListenVerb { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                var help = new HelpText
+                {
+                    Heading = new HeadingInfo("SeudoBuild", "1.0"),
+                    AdditionalNewLineAfterOption = true,
+                    AddDashesToOption = true
+                };
+                //help.AddPreOptionsLine("Usage: app -p Someone");
+                help.AddOptions(this);
+                return help;
+            }
         }
 
         class BuildSubOptions
@@ -52,8 +66,15 @@ namespace SeudoBuild.Agent
         {
             string invokedVerb = null;
             object invokedVerbInstance = null;
-
             var options = new Options();
+
+            if (args.Length == 0)
+            {
+                string help = options.GetUsage();
+                Console.WriteLine(help);
+                return;
+            }
+
             var parseSuccess = Parser.Default.ParseArguments(args, options, (verb, subOptions) =>
             {
                 // if parsing succeeds the verb name and correct instance
