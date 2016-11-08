93d276f512a16e1aa759844b580d5e1a13b19672
Michael Stevenson <michael@mstevenson.net>
2016-11-08 00:57:59 -0800

    Build agent command line options, build queue scaffolding

---
+++ SeudoBuild.Agent/BuildQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace SeudoBuild.Agent
+{
+    public class BuildQueue : IDisposable
+    {
+        CancellationTokenSource cancelTokenSource;
+        CancellationToken cancelToken;
+
+        public void Start()
+        {
+            Console.WriteLine("Starting server");
+            cancelTokenSource = new CancellationTokenSource();
+            cancelToken = cancelTokenSource.Token;
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("  Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         noAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
+            //var task = Task.Factory.StartNew(ServerTask, cancelToken, TaskCreationOptions.LongRunning, null);
+        }
+
+        void ServerTask()
+        {
+            try
+            {
+                while (true)
+                {
+                    cancelToken.ThrowIfCancellationRequested();
+
+                    // TODO
+                }
+            }
+            finally
+            {
+                // TODO cleanup
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping server");
+            cancelTokenSource.Cancel();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
--- SeudoBuild.Agent/Program.cs
+++ SeudoBuild.Agent/Program.cs
@@ -1,68 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 using SeudoBuild;
-using Mono.Options;
+using CommandLine;
+using CommandLine.Text;
 
 namespace SeudoBuild.Agent
 {
     class MainClass
     {
+        class Options
+        {
+            [VerbOption("build", HelpText = "Create a local build.")]
+            public BuildSubOptions BuildVerb { get; set; }
+
+            [VerbOption("submit", HelpText = "Submit a build request for a remote build agent to fulfill.")]
+            public SubmitSubOptions SubmitVerb { get; set; }
+
+            [VerbOption("listen", HelpText = "Listen for and fulfill build requests.")]
+            public ListenSubOptions ListenVerb { get; set; }
+        }
+
+        class BuildSubOptions
+        {
+            [Option('o', "output-folder", HelpText = "Path to the build output folder.", Required = true)]
+            public string OutputPath { get; set; }
+
+            [Option('p', "project-config", HelpText = "Path to a project configuration file.", Required = true)]
+            public string ProjectConfigPath { get; set; }
+
+            [Option('t', "build-target", HelpText = "Name of the target to build as specified in the project configuration file.", Required = true)]
+            public string BuildTarget { get; set; }
+        }
+
+        class ListenSubOptions
+        {
+        }
+
+        class SubmitSubOptions
+        {
+            [Option('p', "project-config", HelpText = "Path to a project configuration file.", Required = true)]
+            public string ProjectConfigPath { get; set; }
+
+            [Option('t', "build-target", HelpText = "Name of the target to build as specified in the project configuration file.", Required = true)]
+            public string BuildTarget { get; set; }
+
+            [Option('h', "host", HelpText = "URI for a specific build agent. If not set, the job will be broadcast to all available agents.")]
+            public string Host { get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            string outputPath = null;
-            string projectConfigPath = null;
-            string buildTarget = null;
+            string invokedVerb = null;
+            object invokedVerbInstance = null;
 
-            var options = new OptionSet
+            var options = new Options();
+            var parseSuccess = Parser.Default.ParseArguments(args, options, (verb, subOptions) =>
             {
-                { "o|output-path=", "a path to the build output folder", o => outputPath = o },
-                { "p|project-config=", "the path to a project configuration json file", p => projectConfigPath = p },
-                { "t|build-target=", "the name of the target to build", t => buildTarget = t },
-            };
+                // if parsing succeeds the verb name and correct instance
+                // will be passed to onVerbCommand delegate (string,object)
+                invokedVerb = verb;
+                invokedVerbInstance = subOptions;
+            });
 
-            List<string> extra;
-            try
+            if (!parseSuccess)
             {
-                extra = options.Parse(args);
-            }
-            catch (OptionException e)
-            {
-                Console.WriteLine("Options:");
-                options.WriteOptionDescriptions(Console.Out);
+                Environment.Exit(Parser.DefaultExitCodeFail);
             }
 
-            if (args.Length == 0)
+            // Build locally
+            if (invokedVerb == "build")
             {
-                var server = new BuildServer();
-                server.Start();
-            }
-            else
-            {
-                if (string.IsNullOrEmpty(projectConfigPath) || string.IsNullOrEmpty(buildTarget) || !string.IsNullOrEmpty(outputPath))
-                {
-                    options.WriteOptionDescriptions(Console.Out);
-                    return;
-                }
-                
-                ProjectConfig projectConfig = null;
-                
+                var buildSubOptions = (BuildSubOptions)invokedVerbInstance;
+                ProjectConfig config = null;
+
                 try
                 {
                     var s = new Serializer();
-                    projectConfig = s.Deserialize<ProjectConfig>(projectConfigPath);
+                    config = s.Deserialize<ProjectConfig>(buildSubOptions.ProjectConfigPath);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Can't parse project config:");
                     Console.WriteLine(e.Message);
                 }
-                
-                if (projectConfig != null)
+
+                if (config != null)
                 {
-                    BuilderConfig builderConfig = new BuilderConfig { ProjectsPath = outputPath };
-                    Builder builder = new Builder(builderConfig);
-                    builder.ExecuteBuild(projectConfig, buildTarget);
+                    //BuilderConfig builderConfig = new BuilderConfig { ProjectsPath = outputPath };
+                    //Builder builder = new Builder(builderConfig);
+                    //builder.ExecuteBuild(projectConfig, buildTarget);
                 }
+            }
+
+            // Submit job to the network
+            if (invokedVerb == "submit")
+            {
+                var submitSubOptions = (SubmitSubOptions)invokedVerbInstance;
+
+                // TODO
+            }
+
+            // Listen for jobs on the network
+            if (invokedVerb == "listen")
+            {
+                var listenSubOptions = (ListenSubOptions)invokedVerbInstance;
+                var server = new BuildQueue();
+                server.Start();
             }
         }
     }
