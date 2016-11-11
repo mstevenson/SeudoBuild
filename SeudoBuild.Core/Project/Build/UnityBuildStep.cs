66165ade44c95eb2bc33214c07fd817d23ebc45c
Michael Stevenson <michael@mstevenson.net>
2016-11-11 00:41:29 -0800

    Parse Unity logs through separate class

--- SeudoBuild.Core/Project/Build/UnityBuildStep.cs
+++ SeudoBuild.Core/Project/Build/UnityBuildStep.cs
@@ -17,8 +17,6 @@ namespace SeudoBuild
         {
             BuildConsole.WriteLine($"Building with Unity {unityInstallation.Version}");
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
             var startInfo = new ProcessStartInfo
             {
                 FileName = unityInstallation.ExePath,
@@ -31,6 +29,8 @@ namespace SeudoBuild
             };
             var unityProcess = new Process { StartInfo = startInfo };
 
+            var logParser = new UnityLogParser();
+
             // FIXME is this all being disposed correctly?
 
             string logPath = GetBuildLogPath(workspace);
@@ -42,7 +42,11 @@ namespace SeudoBuild
                     //Console.WriteLine(e.Data);
                     writer.WriteLine(e.Data);
                     writer.Flush();
-                    HandleLogLine(e.Data);
+                    string logOutput = logParser.ProcessLogLine(e.Data);
+                    if (logOutput != null)
+                    {
+                        BuildConsole.WriteLine(logOutput);
+                    }
                     // TODO examine log for errors and other pertinent info
                 };
 
@@ -59,8 +63,6 @@ namespace SeudoBuild
             {
                 writer.Close();
             }
-
-            Console.ResetColor();
 
             var buildResult = new BuildResult();
             if (unityProcess.ExitCode == 0)
@@ -79,65 +81,6 @@ namespace SeudoBuild
         {
             string now = DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss");
             return $"{workspace.LogsDirectory}/Unity_Build_Log_{now}.txt";
-        }
-
-        // Examine and respond to a line in the Unity log file
-        void HandleLogLine(string line)
-        {
-            if (line == null)
-            {
-                return;
-            }
-
-            if (line.Contains("WARNING: "))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            if (line.Contains("ERROR: "))
-            {
-                BuildConsole.WriteLine(line);
-            }
-
-            if (line.Contains("Initialize mono"))
-            {
-                BuildConsole.WriteLine("Loading Unity project");
-            }
-            if (line.Contains("- starting compile"))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            //if (line.Contains("Compilation succeeded"))
-            //{
-            //    BuildConsole.WriteLine(line);
-            //}
-            if (line.Contains("Compilation failed"))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            if (line.Contains("Finished compile"))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            //if (line.Contains("Launched and connected shader compiler"))
-            //{
-            //    BuildConsole.WriteLine("Running shader compiler");
-            //}
-            //if (line.Contains("Total AssetImport time"))
-            //{
-            //    BuildConsole.WriteLine(line);
-            //}
-            if (line.Contains("building "))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            if (line.Contains("Opening scene "))
-            {
-                BuildConsole.WriteLine(line);
-            }
-            if (line.Contains("*** Completed"))
-            {
-                BuildConsole.WriteLine(line);
-            }
         }
     }
 }
---
+++ SeudoBuild.Core/Project/Build/UnityLogParser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace SeudoBuild
+{
+    public class UnityLogParser
+    {
+        enum State
+        {
+            None,
+            CompilingScripts
+        }
+
+        State state;
+
+        public string ProcessLogLine(string line)
+        {
+            if (line == null)
+            {
+                return null;
+            }
+
+            switch (state)
+            {
+                case State.CompilingScripts:
+                    break;
+                default:
+                    
+                    if (line.Contains("WARNING: "))
+                    {
+                        return line;
+                    }
+                    if (line.Contains("ERROR: "))
+                    {
+                        return line;
+                    }
+                    
+                    if (line.Contains("Initialize mono"))
+                    {
+                        return "Loading Unity project";
+                    }
+                    if (line.Contains("- starting compile"))
+                    {
+                        return line;
+                    }
+                    //if (line.Contains("Compilation succeeded"))
+                    //{
+                    //    BuildConsole.WriteLine(line);
+                    //}
+                    if (line.Contains("Compilation failed"))
+                    {
+                        return line;
+                    }
+                    if (line.Contains("Finished compile"))
+                    {
+                        return line;
+                    }
+                    //if (line.Contains("Launched and connected shader compiler"))
+                    //{
+                    //    BuildConsole.WriteLine("Running shader compiler");
+                    //}
+                    //if (line.Contains("Total AssetImport time"))
+                    //{
+                    //    BuildConsole.WriteLine(line);
+                    //}
+                    if (line.Contains("building "))
+                    {
+                        return line;
+                    }
+                    if (line.Contains("Opening scene "))
+                    {
+                        return line;
+                    }
+                    if (line.Contains("*** Completed"))
+                    {
+                        return line;
+                    }
+                    break;
+            }
+            return null;
+        }
+    }
+}
