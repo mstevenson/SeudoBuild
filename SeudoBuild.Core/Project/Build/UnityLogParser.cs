f3e4c71646f592ad24f260ccdd6af19433829fe7
Michael Stevenson <michael@mstevenson.net>
2016-11-12 00:47:50 -0800

    Cleaner log file parser syntax

--- SeudoBuild.Core/Project/Build/UnityLogParser.cs
+++ SeudoBuild.Core/Project/Build/UnityLogParser.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//using StringMatch = System.Collections.Generic.Dictionary<string, System.Func<string, string>>;
+
 namespace SeudoBuild
 {
     public class UnityLogParser
@@ -6,75 +11,123 @@ namespace SeudoBuild
         enum State
         {
             None,
-            CompilingScripts
+            Start,
+            CompilingScripts,
+            Cancelled
         }
 
-        State state;
+        State currentState = State.Start;
+
+        //delegate string MatchDelegate(string line);
+
+        //Dictionary<string, MatchDelegate> generalMatches = new Dictionary<string, MatchDelegate>();
+        //Dictionary<State, Dictionary<string, MatchDelegate>> stateMaches = new Dictionary<State, Dictionary<string, MatchDelegate>>();
+
+        Matches firstMatches = new Matches();
+        Dictionary<State, Matches> stateMatches = new Dictionary<State, Matches>();
+        Matches lastMatches = new Matches();
+
+
+        public class Match
+        {
+            public readonly string text;
+            public readonly Func<string, string> output;
+
+            public Match(string text, Func<string, string> output)
+            {
+                this.text = text;
+                this.output = output;
+            }
+        }
+
+        public class Matches : IEnumerable<Match>
+        {
+            readonly List<Match> matches = new List<Match>();
+
+            public void Add(string text, Func<string, string> output)
+            {
+                matches.Add(new Match(text, output));
+            }
+
+            public IEnumerator<Match> GetEnumerator()
+            {
+                return matches.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+
+        public UnityLogParser()
+        {
+            firstMatches = new Matches
+            {
+                { "WARNING: ", (line) => line },
+                { "ERROR: ", (line) => line },
+                { "Aborting batchmode due to failure", (line) => line }
+            };
+
+            stateMatches = new Dictionary<State, Matches>
+            {
+                { State.Start, new Matches
+                    {
+                        { "Initialize mono", (line) => { currentState = State.None; return "Loading Unity project"; } }
+                    }
+                },
+                { State.Cancelled, new Matches
+                    {
+                        { "is an incorrect path for a scene file", (line) => { return line; } }
+                    }
+                }
+            };
+
+            lastMatches = new Matches
+            {
+                { "- starting compile", (line) => line },
+                { "Compilation failed", (line) => line },
+                { "Finished compile", (line) => line },
+                { "building ", (line) => line },
+                { "*** Cancelled", (line) => { currentState = State.Cancelled; return "Cancelled build"; } },
+                { "*** Completed", (line) => line }
+            };
+        }
 
         public string ProcessLogLine(string line)
         {
-            if (line == null)
+            foreach (var match in firstMatches)
             {
-                return null;
+                if (line.Contains(match.text))
+                {
+                    string result = match.output.Invoke(line);
+                    return result;
+                }
             }
 
-            switch (state)
+            Matches matches = null;
+            if (stateMatches.TryGetValue(currentState, out matches))
             {
-                case State.CompilingScripts:
-                    break;
-                default:
-                    
-                    if (line.Contains("WARNING: "))
+                foreach (var match in matches)
+                {
+                    if (line.Contains(match.text))
                     {
-                        return line;
+                        string result = match.output.Invoke(line);
+                        return result;
                     }
-                    if (line.Contains("ERROR: "))
-                    {
-                        return line;
-                    }
-                    
-                    if (line.Contains("Initialize mono"))
-                    {
-                        return "Loading Unity project";
-                    }
-                    if (line.Contains("- starting compile"))
-                    {
-                        return line;
-                    }
-                    //if (line.Contains("Compilation succeeded"))
-                    //{
-                    //    BuildConsole.WriteLine(line);
-                    //}
-                    if (line.Contains("Compilation failed"))
-                    {
-                        return line;
-                    }
-                    if (line.Contains("Finished compile"))
-                    {
-                        return line;
-                    }
-                    //if (line.Contains("Launched and connected shader compiler"))
-                    //{
-                    //    BuildConsole.WriteLine("Running shader compiler");
-                    //}
-                    //if (line.Contains("Total AssetImport time"))
-                    //{
-                    //    BuildConsole.WriteLine(line);
-                    //}
-                    if (line.Contains("building "))
-                    {
-                        return line;
-                    }
-                    if (line.Contains("Opening scene "))
-                    {
-                        return line;
-                    }
-                    if (line.Contains("*** Completed"))
-                    {
-                        return line;
-                    }
-                    break;
+                }
             }
+
+            foreach (var match in lastMatches)
+            {
+                if (line.Contains(match.text))
+                {
+                    string result = match.output.Invoke(line);
+                    return result;
+                }
+            }
+
             return null;
         }
     }
