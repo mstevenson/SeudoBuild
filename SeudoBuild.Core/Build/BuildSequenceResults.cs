7b512b21d81cac8000ee1e31756655e53ef2b3ba
Michael Stevenson <michael@mstevenson.net>
2016-11-12 17:31:35 -0800

    Cleanup

--- SeudoBuild.Core/Build/BuildSequenceResults.cs
+++ SeudoBuild.Core/Build/BuildSequenceResults.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SeudoBuild
+﻿namespace SeudoBuild
 {
     public class BuildSequenceResults : PipelineSequenceResults<BuildStepResults>
     {
-        public string ProjectName { get; set; } = string.Empty;
-        public string BuildTargetName { get; set; } = string.Empty;
-        public DateTime BuildDate { get; set; } = DateTime.Now;
-        public TimeSpan BuildDuration { get; set; } = new TimeSpan();
-        public string CommitIdentifier { get; set; } = string.Empty;
-        public VersionNumber AppVersion { get; set; } = new VersionNumber();
     }
 }
