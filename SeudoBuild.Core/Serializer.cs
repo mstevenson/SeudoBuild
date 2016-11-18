57c6fc60047a8a7279b684fc0de7cfb85d62c39d
Michael Stevenson <michael@mstevenson.net>
2016-11-18 01:06:31 -0800

    Delete existing serialized file before writing new file

--- SeudoBuild.Core/Serializer.cs
+++ SeudoBuild.Core/Serializer.cs
@@ -25,6 +25,10 @@ namespace SeudoBuild
 
         public void Serialize<T>(T obj, string path)
         {
+            if (fileSystem.FileExists(path))
+            {
+                fileSystem.DeleteFile(path);
+            }
             using (TextWriter tw = new StreamWriter(fileSystem.OpenWrite(path)))
             {
                 string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
