19d0dca26ee1cbeb7e5771d61b7d114baeb54cce
Michael Stevenson <michael@mstevenson.net>
2016-11-23 11:08:10 -0800

    Formatting

--- SeudoBuild.Agent/Discovery/UdpDiscoveryClient.cs
+++ SeudoBuild.Agent/Discovery/UdpDiscoveryClient.cs
@@ -6,34 +6,38 @@ using System.Net;
 
 namespace SeudoBuild.Net
 {
-	/// <summary>
-	/// Listens for UDP beacon packets from UdpDiscoveryServer.
-	/// </summary>
+    /// <summary>
+    /// Listens for UDP beacon packets from UdpDiscoveryServer.
+    /// </summary>
     public class UdpDiscoveryClient : IDisposable
-	{
-		const ushort multicastPort = 6767;
-		const string multicastAddress = "239.17.0.1";
+    {
+        const ushort multicastPort = 6767;
+        const string multicastAddress = "239.17.0.1";
 
-		UdpClient udpClient;
-		IPEndPoint endPoint;
-		Thread networkThread;
-		List<ServerInfo> servers = new List<ServerInfo> ();
+        UdpClient udpClient;
+        IPEndPoint endPoint;
+        Thread networkThread;
+        List<ServerInfo> servers = new List<ServerInfo>();
 
         public event Action<ServerInfo> ServerFound;
         public event Action<ServerInfo> ServerLost;
 
         public bool IsRunning { get; protected set; }
 
-		public ServerInfo[] AvailableServers
-		{
-			get {
-				if (servers.Count == 0) {
-					return new ServerInfo[0];
-				} else {
-					return servers.ToArray ();
-				}
-			}
-		}
+        public ServerInfo[] AvailableServers
+        {
+            get
+            {
+                if (servers.Count == 0)
+                {
+                    return new ServerInfo[0];
+                }
+                else
+                {
+                    return servers.ToArray();
+                }
+            }
+        }
 
         public void OnServerFound(ServerInfo server)
         {
@@ -52,81 +56,91 @@ namespace SeudoBuild.Net
         }
 
 
-		public void Initialize ()
-		{
-			udpClient = new UdpClient ();
-			var localEndPoint = new IPEndPoint (IPAddress.Any, multicastPort);
-			udpClient.Client.Bind (localEndPoint);
-			var ip = IPAddress.Parse (multicastAddress);
-			udpClient.MulticastLoopback = true;
-			udpClient.JoinMulticastGroup (ip);
-		}
+        public void Initialize()
+        {
+            udpClient = new UdpClient();
+            var localEndPoint = new IPEndPoint(IPAddress.Any, multicastPort);
+            udpClient.Client.Bind(localEndPoint);
+            var ip = IPAddress.Parse(multicastAddress);
+            udpClient.MulticastLoopback = true;
+            udpClient.JoinMulticastGroup(ip);
+        }
 
-		public void Start ()
-		{
-			Console.WriteLine ("Server Discovery Started: " + multicastAddress + ":" + multicastPort);
-			IsRunning = true;
-			networkThread = new Thread (new ThreadStart (NetworkThread));
-			networkThread.IsBackground = true;
-			networkThread.Start ();
-		}
+        public void Start()
+        {
+            Console.WriteLine("Server Discovery Started: " + multicastAddress + ":" + multicastPort);
+            IsRunning = true;
+            networkThread = new Thread(new ThreadStart(NetworkThread));
+            networkThread.IsBackground = true;
+            networkThread.Start();
+        }
 
-		public void Stop ()
-		{
-			Console.WriteLine ("Server Discovery Stopped");
-			IsRunning = false;
-			if (udpClient != null) {
-				udpClient.Close ();
-			}
-		}
+        public void Stop()
+        {
+            Console.WriteLine("Server Discovery Stopped");
+            IsRunning = false;
+            if (udpClient != null)
+            {
+                udpClient.Close();
+            }
+        }
 
-		void NetworkThread ()
-		{
-			//			try {
-			while (IsRunning) {
-				byte[] data = udpClient.Receive (ref endPoint);
-				ServerInfo serverInfo = ServerInfo.FromBytes (data);
-				serverInfo.address = endPoint.Address;
-				if (serverInfo != null) {
-					bool serverExists = false;
-					foreach (var s in servers) {
-						if (s == serverInfo) {
-							s.lastSeen = DateTime.Now;
-							serverExists = true;
-						}
-					}
-					if (!serverExists) {
-						servers.Add (serverInfo);
-						OnServerFound (serverInfo);
-					}
-					servers = PruneLostServers (servers);
-				}
-			}
-			//			} catch {}
-			IsRunning = false;
-		}
+        void NetworkThread()
+        {
+            //			try {
+            while (IsRunning)
+            {
+                byte[] data = udpClient.Receive(ref endPoint);
+                ServerInfo serverInfo = ServerInfo.FromBytes(data);
+                serverInfo.address = endPoint.Address;
+                if (serverInfo != null)
+                {
+                    bool serverExists = false;
+                    foreach (var s in servers)
+                    {
+                        if (s == serverInfo)
+                        {
+                            s.lastSeen = DateTime.Now;
+                            serverExists = true;
+                        }
+                    }
+                    if (!serverExists)
+                    {
+                        servers.Add(serverInfo);
+                        OnServerFound(serverInfo);
+                    }
+                    servers = PruneLostServers(servers);
+                }
+            }
+            //			} catch {}
+            IsRunning = false;
+        }
 
-		List<ServerInfo> PruneLostServers (List<ServerInfo> servers, int timeout = 3500)
-		{
-			List<ServerInfo> pruned = new List<ServerInfo> ();
-			var now = DateTime.Now;
-			foreach (var serverInfo in servers) {
-				var span = serverInfo.lastSeen.Subtract (now);
-				if (span.Milliseconds < timeout) {
-					pruned.Add (serverInfo);
-				} else {
-					OnServerLost (serverInfo);
-				}
-			}
-			return pruned;
-		}
+        List<ServerInfo> PruneLostServers(List<ServerInfo> servers, int timeout = 3500)
+        {
+            List<ServerInfo> pruned = new List<ServerInfo>();
+            var now = DateTime.Now;
+            foreach (var serverInfo in servers)
+            {
+                var span = serverInfo.lastSeen.Subtract(now);
+                if (span.Milliseconds < timeout)
+                {
+                    pruned.Add(serverInfo);
+                }
+                else
+                {
+                    OnServerLost(serverInfo);
+                }
+            }
+            return pruned;
+        }
 
-		public void Dispose ()
-		{
-			IsRunning = false;
-			udpClient.Close ();
-		}
+        public void Dispose()
+        {
+            IsRunning = false;
+            udpClient.Close();
+        }
 
-	}
-	
+    }
+
 }
--- SeudoBuild.Agent/Discovery/UdpDiscoveryServer.cs
+++ SeudoBuild.Agent/Discovery/UdpDiscoveryServer.cs
@@ -6,69 +6,85 @@ using System.Net;
 namespace SeudoBuild.Net
 {
     public class UdpDiscoveryServer : IDisposable
-	{
+    {
         ServerInfo server;
 
-		const int broadcastDelay = 1000; // milliseconds
+        const int broadcastDelay = 1000; // milliseconds
 
-		const ushort multicastPort = 6767;
-		const string multicastAddress = "239.17.0.1";
+        const ushort multicastPort = 6767;
+        const string multicastAddress = "239.17.0.1";
 
-		UdpClient udpClient;
-		IPEndPoint endPoint;
-		Thread networkThread;
+        UdpClient udpClient;
+        IPEndPoint endPoint;
+        Thread networkThread;
+
+        bool isInitialized;
 
         public bool IsRunning { get; protected set; }
 
         public UdpDiscoveryServer(ServerInfo server)
         {
             this.server = server;
+        }
 
+        public void Start()
+        {
+            if (IsRunning)
+            {
+                return;
+            }
+
+            // Initialize
             udpClient = new UdpClient();
             var ip = IPAddress.Parse(multicastAddress);
             endPoint = new IPEndPoint(ip, multicastPort);
             udpClient.MulticastLoopback = true;
             udpClient.JoinMulticastGroup(ip);
+
+            // Run
+            Console.WriteLine("Server beacon started: port " + multicastPort);
+            IsRunning = true;
+            networkThread = new Thread(new ThreadStart(NetworkThread));
+            networkThread.IsBackground = true;
+            networkThread.Start();
         }
 
-		public void Start ()
-		{
-			Console.WriteLine ("Server beacon started: port " + multicastPort);
-			IsRunning = true;
-			networkThread = new Thread (new ThreadStart (NetworkThread));
-			networkThread.IsBackground = true;
-			networkThread.Start ();
-		}
+        public void Stop()
+        {
+            if (!IsRunning)
+            {
+                return;
+            }
+            Console.WriteLine("Server beacon stopped");
+            IsRunning = false;
+            if (udpClient != null)
+            {
+                udpClient.Close();
+                udpClient.Dispose();
+            }
+        }
 
-		public void Stop ()
-		{
-			Console.WriteLine ("Server beacon stopped");
-			IsRunning = false;
-			if (udpClient != null) {
-				udpClient.Close ();
-			}
-		}
+        void NetworkThread()
+        {
+            //			try {
+            while (IsRunning)
+            {
+                // Broadcast server beacon to all clients on the local network
+                byte[] message = ServerInfo.ToBytes(server);
+                udpClient.Send(message, message.Length, endPoint);
+                //				Console.WriteLine ("Broadcasted");
+                Thread.Sleep(broadcastDelay);
+            }
+            //			} catch {}
+            IsRunning = false;
+        }
 
-		void NetworkThread ()
-		{
-			//			try {
-			while (IsRunning) {
-				// Broadcast server beacon to all clients on the local network
-				byte[] message = ServerInfo.ToBytes (server);
-				udpClient.Send (message, message.Length, endPoint);
-//				Console.WriteLine ("Broadcasted");
-				Thread.Sleep (broadcastDelay);
-			}
-			//			} catch {}
-			IsRunning = false;
-		}
-
-		public void Dispose ()
-		{
-			IsRunning = false;
-			udpClient.Close ();
-		}
-	}
+        public void Dispose()
+        {
+            IsRunning = false;
+            udpClient.Close();
+        }
+    }
 
 }
 
