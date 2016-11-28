1af280be382043a34d1936b74f7967dfe1411201
Michael Stevenson <michael@mstevenson.net>
2016-11-28 15:07:16 -0800

    Added projects for desktop app

---
+++ Clients/SeudoBuild.Client.Desktop.XamMac2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto;
+using Eto.Forms;
+
+namespace SeudoBuild.Client.Desktop.XamMac2
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Platforms.XamMac2).Run(new MainForm());
+        }
+    }
+}
